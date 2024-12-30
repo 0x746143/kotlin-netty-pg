aa46a3a2053a0f8657b56559d027f76ca8da6aeb
0x746143 <176037012+0x746143@users.noreply.github.com>
2024-12-30 16:17:01 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "com.github.x746143"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
